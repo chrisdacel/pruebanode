3f64b9098cfb41ec83c6a5a35ca3cbc5eaef91a5
Cristian David Acevedo Lozano <cristian_dacevedol@soy.sena.edu.co>
2025-11-07 07:31:05 -0500

    ultimos cambios

--- config/db.js
+++ config/db.js
@@ -2,20 +2,25 @@ import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
 dotenv.config({path: '.env'});
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER,process.env.DB_PASS ?? '', {
-    host: process.env.DB_HOST,
-    port: process.env.DB_HOSTPORT,
-    dialect: 'mysql',
-    define: {
-        timestamps: true
-    },
-    pull: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-    },
-    operatorsAliases: true,
-});
+const db = new Sequelize(
+    process.env.DB_NAME, 
+    process.env.DB_USER,
+    process.env.DB_PASS ?? '',
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_HOSTPORT,
+        dialect: 'mysql',
+        define: {
+            timestamps: true
+        },
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000,
+        },
+        operatorsAliases: true,
+    }
+);
 
 export default db;
--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -1,38 +1,96 @@
-import { check, validationResult } from 'express-validator'
+import { check, validationResult } from "express-validator";
+import { generarId } from "../helpers/tokens.js";
+import Usuario from "../models/Usuario.js";
 
-import Usuario from '../models/Usuario.js'
+const formularioLogin = (req, res) => {
+  res.render("auth/login", {
+    tituloPagina: "Inicio de Sesión",
+  });
+};
 
-const formularioLogin = (req,res) => {
-        res.render('auth/login', {
-            tituloPagina: 'Inicio de Sesion',
-        })
-    }
+const formularioRegistro = (req, res) => {
+  res.render("auth/registro", {
+    tituloPagina: "Registro de Usuario",
+  });
+};
 
-const formularioRegistro = (req,res) => {
-        res.render('auth/register', {
-            tituloPagina: 'Registro de usuario',
-        })
-    }
+const registrar = async (req, res) => {
+    
+  // Validaciones
+  await check("nombre")
+    .notEmpty()
+    .withMessage("El nombre no puede estar vacio")
+    .run(req);
 
-const registrar = async (req,res) => {
-    // Validaciones
-    await check('nombre').notEmpty().withMessage('El nombre no puede estar vacio').run(req);
-    await check('email').isEmail().withMessage('Esto no parece un correo').run(req);
-    await check('password').isLength({min: 6}).withMessage('La contraseña debe ser al menos de 6 caracteres').run(req);
-    await check('repeat_password').equals(req.body.password).withMessage('La contraseña no es igual').run(req);
+  await check("email")
+    .isEmail()
+    .withMessage("Esto no parece un correo")
+    .run(req);
 
-    // Verificar que el resultado este vacio
-    // if(resultado.array())
+  await check("password")
+    .isLength({ min: 6 })
+    .withMessage("La contraseña debe ser al menos de 6 caracteres")
+    .run(req);
 
-    let resultado = validationResult(req);
-    res.json(resultado.array());
+  await check("repeat_password")
+    .equals(req.body.password)
+    .withMessage("La contraseña no es igual")
+    .run(req);
 
-    // const usuarios = await Usuario.create(req.body);
-}
+  let resultado = validationResult(req);
 
-const formularioOlvidePassword = (req,res) => {
-    res.render('auth/olvide-password', {
-        tituloPagina: 'Olvide contraseña',
-    })
-}
-export {formularioLogin, registrar, formularioRegistro, formularioOlvidePassword};
+  // Verificar que el resultado este vacio
+  if (!resultado.isEmpty()) {
+    // Errores
+    return res.render("auth/register", {
+      tituloPagina: "Registro de Usuario",
+      errores: resultado.array(),
+      usuario: {
+        nombre: req.body.nombre,
+        email: req.body.email,
+      },
+    });
+  }
+  
+  // Extraer los datos
+  const {nombre, email, password} = req.body;
+
+  // Validar si el correo ya existe
+  const existeUsuario = await Usuario.findOne({where: {email: req.body.email}})
+  if (existeUsuario) {
+    return res.render('auth/register' ,{
+      tituloPagina: 'Registro de Usuario',
+      errores:[{msg: 'El usuario ya existe'}],
+      usuario:{
+        nombre: req.body.Nombre,
+        correo: req.body.email,
+      }
+    });
+  }
+
+  const usuarios = await Usuario.create({
+    nombre,
+    email,
+    password,
+    token: generarId(),
+  });
+
+  // Mostrar mensaje de Confirmación
+  res.render("templates/mensaje", {
+    tituloPagina: "Cuenta Creada",
+    mensaje: "Se ha enviado un correo de confirmación, Da clic en el enlace.",
+  });
+};
+
+const formularioOlvidePassword = (req, res) => {
+  res.render("auth/olvide-password", {
+    tituloPagina: "Olvide Contraseña",
+  });
+};
+
+export {
+  formularioLogin,
+  formularioRegistro,
+  registrar,
+  formularioOlvidePassword,
+};
