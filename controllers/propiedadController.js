fd99339c1bac44ac192a60f5ebc30ac88622ee5b
Cristian David Acevedo Lozano <cristian_dacevedol@soy.sena.edu.co>
2025-11-25 08:53:48 -0500

    agrego vistas de propiedades, correccion de errores

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,11 +1,55 @@
 import { Precios, Categorias, Usuarios, Propiedades } from "../models/index.js";
 import { validationResult } from "express-validator";
 
-const admin = (req, res) => {
-  res.render("propiedades/admin", {
-    tituloPagina: "Mis propiedades",
-    csrfToken: req.csrfToken(),
-  });
+const admin = async (req, res) => {
+  // Leer QueryString
+  const { pagina: paginaActual } = req.query;
+
+  const expresion = /^[1-9]$/;
+
+  if (!expresion.test(paginaActual)) {
+    return res.redirect("/mis-propiedades?pagina=1");
+  }
+
+  try {
+    const { id } = req.usuario;
+
+    // Limites y Offset para el paginador
+    const limit = 10;
+    const offset = paginaActual * limit - limit;
+
+    const [propiedades, total] = await Promise.all([
+      Propiedades.findAll({
+        limit,
+        offset,
+        where: {
+          usuarioId: id,
+        },
+        include: [
+          { model: Categorias, as: "categoria" },
+          { model: Precios, as: "precio" },
+        ],
+      }),
+      Propiedades.count({
+        where: {
+          usuarioId: id,
+        },
+      }),
+    ]);
+
+    res.render("propiedades/admin", {
+      pagina: "Mis Propiedades",
+      propiedades,
+      csrfToken: req.csrfToken(),
+      paginas: Math.ceil(total / limit),
+      paginaActual: Number(paginaActual),
+      total,
+      offset,
+      limit,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const crear = async (req, res) => {
@@ -87,12 +131,59 @@ const guardar = async (req, res) => {
 };
 
 const agregarImagen = async (req, res) => {
+  const { id } = req.params;
+  // Validar que la propiedad exista
+  const propiedad = await Propiedades.findByPk(id);
+  if (!propiedad) {
+    return res.redirect("/mis-propiedades");
+  }
+
+  // Validar si esta publicada
+  if (propiedad.publicado) {
+    return res.redirect("/mis-propiedades");
+  }
+
+  //Validar que la propiedad sea del usuario
+  if (req.usuario.id.toString() !== propiedad.usuarioId.toString()) {
+    return res.redirect("/mis-propiedades");
+  }
+
   res.render("propiedades/agregar-imagen", {
     tituloPagina: "Agregar una imagen",
     csrfToken: req.csrfToken(),
+    propiedad,
   });
-
-  console.log(req.params);
 };
 
-export { admin, crear, guardar, agregarImagen };
+const almacenarImagen = async (req, res, next) => {
+  const { id } = req.params;
+  // Validar que la propiedad exista
+  const propiedad = await Propiedades.findByPk(id);
+  if (!propiedad) {
+    return res.redirect("/mis-propiedades");
+  }
+
+  // Validar si esta publicada
+  if (propiedad.publicado) {
+    return res.redirect("/mis-propiedades");
+  }
+
+  //Validar que la propiedad sea del usuario
+  if (req.usuario.id.toString() !== propiedad.usuarioId.toString()) {
+    return res.redirect("/mis-propiedades");
+  }
+
+  try {
+    //Almacenar en la DB
+    propiedad.imagen = req.file.filename;
+    propiedad.publicado = 1;
+
+    await propiedad.save();
+
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { admin, crear, guardar, agregarImagen, almacenarImagen };
