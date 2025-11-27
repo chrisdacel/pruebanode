b51eae6f7ae3134695f5130d0760dd04fe214915
Cristian David Acevedo Lozano <cristian_dacevedol@soy.sena.edu.co>
2025-11-27 10:47:36 -0500

    añado funcion de editar y eliminar la publicacion

--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -6,6 +6,10 @@ import {
   guardar,
   agregarImagen,
   almacenarImagen,
+  editar,
+  guardarCambios,
+  eliminar,
+  cambiarEstado,
 } from "../controllers/propiedadController.js";
 import protegerRuta from "../middleware/protegerRuta.js";
 import upload from "../middleware/subirImagen.js";
@@ -44,5 +48,32 @@ router.post(
   upload.single("imagen"),
   almacenarImagen
 );
+
+router.get("/propiedades/editar/:id", protegerRuta, editar);
+
+router.post(
+  "/propiedades/editar/:id",
+  protegerRuta,
+  body("titulo").notEmpty().withMessage("El Titulo del Anuncio es Obligatorio"),
+  body("descripcion")
+    .notEmpty()
+    .withMessage("La Descripción no puede ir vacia")
+    .isLength({ max: 200 })
+    .withMessage("La Descripción es muy larga"),
+  body("categoria").isNumeric().withMessage("Selecciona una categoría"),
+  body("precio").isNumeric().withMessage("Selecciona un rango de Precios"),
+  body("habitaciones")
+    .isNumeric()
+    .withMessage("Selecciona la Cantidad de Habitaciones"),
+  body("parqueaderos")
+    .isNumeric()
+    .withMessage("Selecciona la Cantidad de Parqueaderos"),
+  body("wc").isNumeric().withMessage("Selecciona la Cantidad de Baños"),
+  body("lat").notEmpty().withMessage("Ubica la Propiedad en el Mapa"),
+  guardarCambios
+);
+
+router.post("/propiedades/eliminar/:id", protegerRuta, eliminar);
+router.put("/propiedades/:id", protegerRuta, cambiarEstado);
 
 export default router;
